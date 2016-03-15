d09e1521daaf45ea3994844218538c8d60114cb8
Don Bosco van Hoi <donbosco.rulz@gmail.com>
2016-03-15 12:06:51 +0100

    Create all_attribute_by_attribute_set.sql

---
+++ attributes/all_attribute_by_attribute_set.sql
@@ -0,0 +1,13 @@
+SELECT ea.attribute_code,
+       ea.attribute_id,
+       ea.backend_model,
+       ea.frontend_input,
+       ea.is_user_defined,
+       ea.is_required
+FROM   eav_attribute_set eas
+       LEFT JOIN eav_entity_attribute eea
+              ON eas.`attribute_set_id` = eea.`attribute_set_id`
+       LEFT JOIN eav_attribute ea
+              ON eea.`attribute_id` = ea.attribute_id
+WHERE  eas.`attribute_set_id` = 4
+   AND eas.`entity_type_id` = 4;
